e58010abbf8aaaf4957b8dffc5cbbdf7a1385daf
brfyamada <bfyamada@outlook.com>
2023-02-14 20:33:07 -0300

    Creating project with test class

---
+++ src/test/java/comn/brfyamada/reactive/test/MonoTest.java
@@ -0,0 +1,28 @@
+package comn.brfyamada.reactive.test;
+
+import lombok.extern.slf4j.Slf4j;
+import org.junit.jupiter.api.Test;
+
+@Slf4j
+/**
+ * Reactive Streams
+ * 1. Asynchronous
+ * 2. Non-blocking
+ * 3. Backpressure
+ * Publisher <- (subscribe) Subscriber
+ * Subscription is created
+ * Publisher (onSubscribe with subscription) -> Subscriber
+ * Subscription <- (request N) Subscriber
+ * until:
+ * 1. Publisher sends all the objects requested
+ * 2. Publisher sends all the objects it has. (onComplete) subscriber and subscription will be canceled
+ * 3. There is an error. (onError) -> subscriber and subbscription will be canceled
+ *
+ */
+public class MonoTest {
+
+    @Test
+    public void test(){
+        log.info("Everything working as intended");
+    }
+}
